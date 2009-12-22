c5b81a4d7adcc883df3ce14dd977489aad672082
Knut Saua Mathiesen <git@canutes.net>
2009-12-22 20:01:55 +0100

    Added the leiningen build files

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject httpserver "0.1-SNAPSHOT" 
+            :description "A simple http-server/framework written in clojure" 
+            :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] 
+                           [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
